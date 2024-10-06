71b4d4e71a61dc7229c847a65855ce9ec0ba1460
Rizki Budi <rizkibudi513@gmail.com>
2024-10-06 10:00:38 +0700

    config: setup tailwind

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,5 +1,13 @@
 <template>
-    <button @click="counter++">Clicked {{ counter }} times</button>
+    <main class="flex flex-col gap-4 justify-center items-center min-h-screen">
+        <h1>Counter: {{ counter }}</h1>
+        <button class="p-2 bg-green-500 text-white rounded" @click="counter++">
+            Increment
+        </button>
+        <button class="p-2 bg-red-500 text-white rounded" @click="counter = 0">
+            Reset
+        </button>
+    </main>
 </template>
 
 <script setup>
